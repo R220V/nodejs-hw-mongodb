ba5edacfacec07a32d02308a50bc460293f29b3b
Krus <krus.margo@gmail.com>
2025-05-26 23:18:00 +0300

    add404

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -31,19 +31,24 @@ export const getContactsController = async (req, res, next) => {
   };
 
 //GETID
-  export const getContactsByIdController = async (req, res, next) => {
+export const getContactsByIdController = async (req, res, next) => {
+  try {
     const { contactId } = req.params;
-    const contact = await getContactById(contactId);   
-    
-	if (!contact) {
-    throw createHttpError('Contact not found');
-	}
+    const contact = await getContactById(contactId);
+
+    if (!contact) {
+      throw createHttpError(404, 'Contact not found');
+    }
+
     res.status(200).json({
-		status:200,
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
-  };
+  } catch (error) {
+    next(error);
+  }
+};
 
   //DELETE
 export const deleteContactsController = async (req, res, next) => {
