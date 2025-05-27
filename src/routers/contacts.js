ae86e049e15e1cc6637052284cea4cf5cadc0578
Krus <krus.margo@gmail.com>
2025-05-28 00:09:42 +0300

    add_isValidID

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,14 +1,18 @@
-import { Router } from 'express';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { 
 	helloRoute, 
 	getContactsController, 
-	getContactsByIdController, deleteContactsController, 
+	getContactsByIdController, 
+	deleteContactsController, 
 	createContactsController,
 	updateContactsController,
 	upsertContactsController
 
 } from '../controllers/contacts.js';
+
+import { Router } from 'express';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidID } from '../middlewares/isValidID.js';
+import { validateBody } from '../middlewares/ValidateBody.js';
 
 
 const router = Router();
@@ -17,14 +21,14 @@ router.get('/', helloRoute);
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
+router.get('/contacts/:contactId', isValidID, ctrlWrapper(getContactsByIdController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactsController));
+router.delete('/contacts/:contactId', isValidID, ctrlWrapper(deleteContactsController));
 
-router.post('/contacts', ctrlWrapper(createContactsController));
+router.post('/contacts', validateBody, ctrlWrapper(createContactsController));
 
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactsController));
+router.patch('/contacts/:contactId', validateBody, isValidID, ctrlWrapper(updateContactsController));
 
-router.put('/contacts/:contactId', ctrlWrapper(upsertContactsController));
+router.put('/contacts/:contactId', isValidID, ctrlWrapper(upsertContactsController));
 
 export default router;
